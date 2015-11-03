1e51798646ffe2f45889a58216eab841f1cd0d5d
Jason Asahara <jasahara@yahoo.com>
2015-11-02 22:48:08 -0800

    database established

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -3,4 +3,24 @@ class DishesController < ApplicationController
   def index
   end
 
+  def new
+    @dish = Dish.new
+  end
+
+  def create
+    @dish = Dish.new(dish_params)
+
+    if @dish.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def dish_params
+      params.require(:dish).permit(:title, :restaurant, :city, :price, :description)
+    end
+
 end
