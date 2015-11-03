5abeb1c7a16a0bb7272f2f0b4dba0067f4a9fb3e
Jason Asahara <jasahara@yahoo.com>
2015-11-03 00:08:26 -0800

    turned to links

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,7 +1,11 @@
 class DishesController < ApplicationController
+  before_action :find_dish, only: [:show, :edit, :update, :destroy]
 
   def index
     @dishes = Dish.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -22,6 +26,10 @@ class DishesController < ApplicationController
 
     def dish_params
       params.require(:dish).permit(:title, :restaurant, :city, :price, :description)
+    end
+
+    def find_dish
+      @dish = Dish.find(params[:id])
     end
 
 end
