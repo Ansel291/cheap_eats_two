6c77eca5359a69a22bbdf30e621a46bfae547b87
Jason Asahara <jasahara@yahoo.com>
2015-11-03 10:57:32 -0800

    create dish image migration and model definition w paperclip

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,3 +1,5 @@
 class Dish < ActiveRecord::Base
   belongs_to :user
+
+  has_attached_file :image, styles: { medium: "300x300>" }
 end
---
+++ db/migrate/20151103185014_add_image_column_to_dishes.rb
@@ -0,0 +1,9 @@
+class AddImageColumnToDishes < ActiveRecord::Migration
+  def up
+    add_attachment :dishes, :image
+  end
+
+  def down
+    remove_attachment :dishes, :image
+  end
+end
