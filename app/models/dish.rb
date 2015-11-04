3d299b7e288d35e1d19247da68121564ea130a99
Jason Asahara <jasahara@yahoo.com>
2015-11-03 19:39:47 -0800

    larger image size

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,6 +2,6 @@ class Dish < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
 
-  has_attached_file :image, styles: { medium: "300x300>" }
+  has_attached_file :image, styles: { medium: "475x475>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
