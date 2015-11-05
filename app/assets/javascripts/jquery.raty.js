e230907f11c92d0ccfa50db0f21fcab81114d5c1
Jason Asahara <jasahara@yahoo.com>
2015-11-05 03:00:14 -0800

    star recovery

--- app/assets/javascripts/jquery.raty.js
+++ app/assets/javascripts/jquery.raty.js
@@ -747,7 +747,7 @@
     space        : true,
     starHalf     : 'https://s3-us-west-1.amazonaws.com/cheapeatsinla/star-half.png',
     starOff      : 'https://s3-us-west-1.amazonaws.com/cheapeatsinla/star-off.png',
-    starOn       : 'https://elephant.png',
+    starOn       : 'https://s3-us-west-1.amazonaws.com/cheapeatsinla/star-on.png',
     starType     : 'img',
     target       : undefined,
     targetFormat : '{score}',
